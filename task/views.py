206b37c0537fe083f63ca16ac95841696175217f
shahab <razavi.shahab@yahoo.com>
2024-01-14 14:26:22 +0330

    form.py added to the project along with its view and html codes

--- task/views.py
+++ task/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import *
+from .forms import *
 
 def index(request):
     tasks = Task.objects.all()
     
-    context = {'tasks': tasks}
+    form = Taskform()
+    
+    if request.method == 'POST':
+        form = Taskform(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'task/list.html', context)
