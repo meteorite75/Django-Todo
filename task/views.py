ac6a74face082ca42998fd0e9febb3adc1285eeb
shahab <razavi.shahab@yahoo.com>
2024-01-15 17:16:29 +0330

    update section added for updating tasks

--- task/views.py
+++ task/views.py
@@ -16,3 +16,18 @@ def index(request):
     
     context = {'tasks': tasks, 'form': form}
     return render(request, 'task/list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    
+    form = Taskform(instance=task)
+    
+    if request.method == 'POST':
+        form = Taskform(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    
+    context = {'form': form}
+    
+    return render(request, 'task/update_task.html', context)
